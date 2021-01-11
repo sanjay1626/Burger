72988f916c1591a81f7fd02bb26d86543ff61958
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-01-10 17:13:52 -0800

    Created Heroku Database

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var dotenv = require('dotenv').config();
 var mysql = require('mysql');
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Stormerdomain012$",
-        database: "burgers_db"
+        host: "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "he1fqgg6zp7jk2jz",
+        password: "ttw582pcvoa14zb6",
+        database: "p0hcejlyusy9rjsa"
     });
 
   //console log if connection made
